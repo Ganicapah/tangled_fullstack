ae128ebeecb5ab1532b95a84588aab5f3b69fe63
Ganicapah <ganivcapah@gmail.com>
2025-01-03 22:11:52 +0700

    setup layout dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,37 @@
+import NavigationBar from "@/components/navbar";
+import db from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await db.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+  // console.log(params);
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <NavigationBar />
+      {children}
+    </>
+  );
+}
